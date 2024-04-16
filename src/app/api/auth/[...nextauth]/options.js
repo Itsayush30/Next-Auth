3c0723c764ce7fff6a8515ac4aae7712ddac8faf
itsayush30 <itsayush30@gmail.com>
2024-04-16 23:47:48 +0530

    Added Signup api with basic setup of next-auth

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,65 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/user";
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connect();
+        try {
+          const user = await User.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before logging in");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
+        } catch {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/sign-in",
+  },
+};
